ad22318cc1f13622c43d14d35bbcb12b971c39c4
dke <dkibui@outlook.com>
2023-07-07 20:00:08 +0300

    Added templating

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, HTTPException, Response, status, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, HTTPException, Request, Response, status, Depends
 from . import schemas, models, db
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 models.Base.metadata.create_all(db.engine)
 
 
-@app.post("/blogs/")
-async def create_blog(blog: schemas.Blog):
-    return blog
+@app.get("/", response_class=HTMLResponse)
+async def create_blog(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
