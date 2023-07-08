5c24f8a9b12145855ee32821ad02fbb568fe384f
dke <dkibui@outlook.com>
2023-07-08 10:46:43 +0300

    Added htmx requests and swap page content on index page

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,10 @@
+from typing import Optional
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, HTTPException, Request, Response, status, Depends
+from fastapi import FastAPI, HTTPException, Request, Response, status, Depends, Header
 from . import schemas, models, db
+
+from .movies import films
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -9,6 +12,8 @@ models.Base.metadata.create_all(db.engine)
 
 
 @app.get("/", response_class=HTMLResponse)
-async def create_blog(request: Request):
-    context = {"request": request}
+async def create_blog(request: Request, hx_request: Optional[str] = Header(None)):
+    context = {"request": request, "films": films}
+    if hx_request:
+        return templates.TemplateResponse("table.html", context)
     return templates.TemplateResponse("index.html", context)
---
+++ blog/movies.py
@@ -0,0 +1,5 @@
+films = [
+    {"name": "Blade Runner", "director": "Ridley Scott"},
+    {"name": "Pulp Fiction", "director": "Quentin Tarantino"},
+    {"name": "Mulholland", "director": "David Lynch"},
+]
