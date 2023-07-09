648970313fc5af2b86f60d56d08291cd4e915860
dke <dkibui@outlook.com>
2023-07-09 16:07:02 +0300

    HTMX get requests

--- film/db.py
+++ film/db.py
@@ -1,19 +1,27 @@
+import time
 import psycopg2
 from psycopg2 import Error
+from psycopg2 import extras
+from dotenv import dotenv_values
 
-user = "postgres"
-password = "3786"
-host = "localhost"
-db = "fastapi"
-port = 5432
+# Load environment variables from .env file
+config = dotenv_values(".env")
 
 
-def db_connection():
+db = config["DB_NAME"]
+user = config["DB_USER"]
+password = config["DB_PASSWORD"]
+host = config["DB_HOST"]
+port = config["DB_PORT"]
+
+while True:
     try:
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             host=host, port=port, database=db, user=user, password=password
         )
+        cursor = conn.cursor(cursor_factory=extras.DictCursor)
         print("Connection to Postgres successful")
-        return connection
-    except (Exception, Error) as error:
+        break
+    except Exception as error:
         print("Error while connecting to PostgreSQL:", error)
+        time.sleep(2)
--- film/main.py
+++ film/main.py
@@ -2,23 +2,36 @@ from typing import Optional
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, HTTPException, Request, Response, status, Depends, Header
-from psycopg2 import extras
 
-from .db import db_connection
+from .db import conn, cursor
 
 templates = Jinja2Templates(directory="templates")
-conn = db_connection()
-cursor = conn.cursor(cursor_factory=extras.DictCursor)
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
-async def create_blog(request: Request, hx_request: Optional[str] = Header(None)):
-    cursor.execute("SELECT * FROM films")
+async def create_blog(
+    request: Request,
+    hx_request: Optional[str] = Header(None),
+    page: int = 1,
+):
+    N = 3
+    OFFSET = (page - 1) * N
+    cursor.execute(
+        """SELECT * FROM public.films ORDER BY id ASC OFFSET %s LIMIT %s""",
+        (
+            OFFSET,
+            N,
+        ),
+    )
     films = cursor.fetchall()
     # cursor.close()
     # conn.close()
-    context = {"request": request, "films": films}
+    context = {
+        "request": request,
+        "films": films,
+        "page": page,
+    }
     if hx_request:
         return templates.TemplateResponse("table.html", context)
     return templates.TemplateResponse("index.html", context)
